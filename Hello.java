6ef4a2c9a8df112efa9c5b3fab6066616a57c37c
madeofsteel1 <maor199979@gmail.com>
2024-12-08 18:49:56 +0200

    changed

--- Hello.java
+++ Hello.java
@@ -1,10 +1,7 @@
-
-
-
 public class Hello{
-public static void main(String[]args){
-for(int i=1;i<=10;i++){
-System.out.println("Hello ..."+i);}
+	public static void main(String[]args){
+			for(int i=1;i<=10;i++){
+					System.out.println("Hello ..."+i);
+			}
+	}
 }
-}
- docker exec jenkins cat/var/jenkins_home/secrets/initialAdminPasswo
